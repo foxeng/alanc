5d2f386bcab0c96dd0f60671e2f12f33de9736b2
Fotis Xenakis <foxen@windowslive.com>
2020-11-07 22:07:12 +0200

    Add elementary command line enhancements

--- main.go
+++ main.go
@@ -9,9 +9,15 @@ import (
 )
 
 func main() {
+	// TODO: Use proper command line parsing packages.
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <source file>\n", os.Args[0])
+		os.Exit(1)
+	}
 	fin, err := os.Open(os.Args[1])
 	if err != nil {
-		return
+		fmt.Fprintf(os.Stderr, "open %q: %v\n", os.Args[1], err)
+		os.Exit(1)
 	}
 	defer fin.Close()
 
